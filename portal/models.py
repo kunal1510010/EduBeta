fdd5c8454e309839330222dda8eae9054b0246c7
kunal1510010 <kunal1510010@gmail.com>
2019-08-30 13:18:09 +0530

    Model Creation

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class UserDetail(models.Model):
+    user_id = models.AutoField(primary_key=True, null=False)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email_id = models.EmailField()
+    password = models.CharField(max_length=20)
+    phone_number = models.IntegerField(null=True)
+
+
+    def __str__(self):
+        return str(self.first_name)
+
--- portal/views.py
+++ portal/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import UploadFileForm
 
-# Create your views here.
+
+def index(request):
+    form = UploadFileForm()
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/success/url/')
+    print(form.as_table())
+    return render(request, 'forms.html', {form: form})
+
+
+def handle_uploaded_file(file):
+    with open('uploads/data_file.csv', 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
+
